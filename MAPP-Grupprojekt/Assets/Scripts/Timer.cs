2c9c45bc9266205c26b6dd0ff0e3c0f1f40b7651
weebchiro <sarita.afshar@gmail.com>
2024-04-09 14:40:04 +0200

    timer done

--- MAPP-Grupprojekt/Assets/Scripts/Timer.cs
+++ MAPP-Grupprojekt/Assets/Scripts/Timer.cs
@@ -7,7 +7,7 @@ public class Timer : MonoBehaviour
 {
     public float timeRemaining = 10;
     public bool timerIsRunning = false;
-    public Text timeText;
+    public Text timerText;
 
     private void Start(){
         timerIsRunning = true;
@@ -16,30 +16,25 @@ public class Timer : MonoBehaviour
    
     void Update()
     {
-       if (timerIsRunning){
-
-        if (timeRemaining > 0){
-           
-            timeRemaining -= Time.deltaTime;
-
-        }
-        else{
-            
-            Debug.Log("Time has run out!");
-            timeRemaining = 0;
-            timerIsRunning = false;
-
+        if (timerIsRunning){
+            if (timeRemaining > 0){
+                timeRemaining -= Time.deltaTime;
+            }
+            else{
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
             }
         }
+        DisplayTime(timeRemaining);
+    }
 
-        void DisplayTime(float timeToDisplay){
+    void DisplayTime(float timeToDisplay){
             
-            timeToDisplay += 1;
 
-            float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-            float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-            timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
-        }
     }
 }
