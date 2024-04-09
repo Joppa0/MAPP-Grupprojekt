d2d7fc0cc8e0253aaca0618a482bdfda68676e0d
Joppa0 <114592889+Joppa0@users.noreply.github.com>
2024-04-09 15:12:15 +0200

    Added player movement
    Player can move the avatar by pressing on the screen. The avatar moves to that point and then stops.

--- MAPP-Grupprojekt/Assets/Scripts/PlayerController.cs
+++ MAPP-Grupprojekt/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float target;
+    [SerializeField] private float speed = 1.0f;
+
+    private bool canMove;
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetTarget();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void SetTarget()
+    {
+        if (Input.touchCount > 0)
+        {
+            // Makes player able to move.
+            canMove = true;
+            // Gets touch position.
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = touch.position;
+
+            // Finds target position to move to.
+            target = Camera.main.ScreenToWorldPoint(touchPos).x;
+        }
+    }
+
+    private void Move()
+    {
+        if (canMove)
+        {
+            // Moves toward target.
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(target, transform.position.y), Time.deltaTime * speed);
+
+            // Stops moving if the target has been reached.
+            if (transform.position.x == target)
+            {
+                canMove = false;
+            }
+        }
     }
 }
