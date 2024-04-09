37387f6bc410b4c160b19644dc6fcdcc7d3eaf86
Julizelo <juleczka.nilsson04@gmail.com>
2024-04-09 15:03:24 +0200

    Bullet testing - klar

--- MAPP-Grupprojekt/Assets/Scripts/MoveForward.cs
+++ MAPP-Grupprojekt/Assets/Scripts/MoveForward.cs
@@ -6,15 +6,37 @@ public class MoveForward : MonoBehaviour
 {
     [SerializeField] private float speed = 1;
 
+    private Rigidbody2D rgbd;
+    private float gravity = 200;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        rgbd = GetComponent<Rigidbody2D>();
+
+        rgbd.AddForce(-Vector2.up * gravity * Time.deltaTime);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        timer += Time.deltaTime;
+
+        if(timer > 5)
+        {
+            Destroy(gameObject);
+        }
+
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+
 }
