80e23808455b6fb252faaa13ef9924592fc87599
Meiling-04 <144385020+Meiling-04@users.noreply.github.com>
2024-04-10 17:14:45 +0200

    Winnig Condition
    Updated the scoresystem script. Now when one counter goes up to 10(adjustable) another scene (of choice) will load within 2 seconds (adjustable)

--- MAPP-Grupprojekt/Assets/Scripts/ScoreManeger.cs
+++ MAPP-Grupprojekt/Assets/Scripts/ScoreManeger.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreManeger : MonoBehaviour
 {
     private int snowballStrikes = 0;
 
     [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private int winningStrikes = 10;
+    [SerializeField] private int levelToLoad;
 
     private void Start()
     {
@@ -17,9 +20,22 @@ public class ScoreManeger : MonoBehaviour
     {
         if (other.CompareTag("Snowball"))
         {
-            Destroy(other.gameObject);
+            
             snowballStrikes++;
             scoreText.text = "" + snowballStrikes;
         }
+    }
+
+    private void Update()
+    {
+        if(snowballStrikes == winningStrikes)
+        {
+            Invoke("LoadNextLevel", 2f);
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
     }
 }
