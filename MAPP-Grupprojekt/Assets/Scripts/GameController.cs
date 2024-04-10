dfbe2bfcf3943fb23942b402f5ae07680798aa3c
Celsiius <hamza1997se@hotmail.com>
2024-04-10 10:21:55 +0200

    Förslag på hur enumet kan se ut

--- MAPP-Grupprojekt/Assets/Scripts/GameController.cs
+++ MAPP-Grupprojekt/Assets/Scripts/GameController.cs
@@ -7,6 +7,11 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public enum BattleState
+    {
+        Player1Move, Player1Throw, Player2Move, Player2Throw,Player1Win, Player2Win // Olika stadier som spelet kan befinna sig i. Tanken är att i varje stadie ska det endast ske det som är menat att ske. Exempel: Player1Move state = spelare 1 ger input för movement.
+    }
+
     // Start is called before the first frame update
     void Start()
     {
