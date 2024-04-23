33a42fc3b7248ba308270712201df03d4d128b77
Meiling-04 <144385020+Meiling-04@users.noreply.github.com>
2024-04-23 19:28:25 +0200

    SkinChangingScript
    skapade ett skinChangingScript. Just nu funkar det att byta karaktär/skin. Dessvärre ändras båda just nu, måste fixa det

---
+++ MAPP-Grupprojekt/Assets/Scripts/SkinChange.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinChange : MonoBehaviour
+{
+
+    public SkinDatabase skinDB;
+
+    public SpriteRenderer artworkSprite;
+
+    private int selectedOption = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("selectedOption"))
+        {
+            selectedOption = 0;
+        }
+        else
+        {
+            Load();
+        }
+
+        UpdateSkin(selectedOption);
+    }
+    private void UpdateSkin(int selectedOption)
+    {
+        Skin skin = skinDB.GetSkin(selectedOption);
+        artworkSprite.sprite = skin.skinSprite;
+    }
+
+    private void Load()
+    {
+        selectedOption = PlayerPrefs.GetInt("selectedOption");
+    }
+}
