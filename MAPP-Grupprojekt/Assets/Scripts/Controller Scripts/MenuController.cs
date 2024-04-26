9df3569baff1f1ce4f8403390586ac3154d76521
Celsiius <hamza1997se@hotmail.com>
2024-04-26 21:29:20 +0200

    Tagit bort mina gamla levlar + Fixat scene hierarchy
    Har fixat vår build Scene index så att det går från Main menu > skin selection > Game
    
    Ifall man vill ändra på vilken level som ska laddas behöver man inte ändra på koden, utan gå till:
    File>Build Settings.
    Längst upp ser man "Scenes in Build".
    Just nu är det följande ordning:
    0: Main Menu
    1: Skin Selection Menu
    2: Win + Replay menu 1.
    
    Det är bara att ta bort scenen från indexet och lägga till en ny ifall man vill att det ska gå en annan ordning / till en annan scen.

--- MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/MenuController.cs
+++ MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/MenuController.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private int levelToLoad;
+    //[SerializeField] private int levelToLoad;
+    public void StartGame()
+    {
+         SceneManager.LoadSceneAsync(1); // Här kan vi skriva in vad leveln heter, "Level 1", ELLER siffran på SceneBuildIndex. När man klickar på Play, ska leveln som står köras.
+    }
+
     public void PlayGame()
     {
-         SceneManager.LoadSceneAsync(levelToLoad); // Här kan vi skriva in vad leveln heter, "Level 1", ELLER siffran på SceneBuildIndex. När man klickar på Play, ska leveln som står köras.
+        SceneManager.LoadSceneAsync(2);
     }
 
     public void QuitGame()
