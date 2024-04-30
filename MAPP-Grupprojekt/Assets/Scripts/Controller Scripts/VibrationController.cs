f327c302be86cdb32b493a5959d95378cab9cae8
Carlroos00 <carl.roos@hotmail.com>
2024-04-30 08:46:22 +0200

    Fix med grafik menyer

--- MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/VibrationController.cs
+++ MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/VibrationController.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 #if UNITY_ANDROID || UNITY_IOS
 using CandyCoded.HapticFeedback;
 #endif
-
+/*
 
 public class VibrationController : MonoBehaviour
 {
@@ -39,4 +39,4 @@ public class VibrationController : MonoBehaviour
 
     
 }
-
+*/
