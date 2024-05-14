c633c2baf4dd23c24fbc98ae306085e348736e47
weebchiro <sarita.afshar@gmail.com>
2024-05-14 13:16:13 +0200

    click

--- MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/VibrationController.cs
+++ MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/VibrationController.cs
@@ -8,33 +8,7 @@ using CandyCoded.HapticFeedback;
 public class VibrationController : MonoBehaviour
 {
 
-    public void DefaultVibration()
-    {
-#if UNITY_ANDROID || UNITY_IOS
-        Debug.Log("Default Vibration performed!");
-            Handheld.Vibrate();
-    }
 
-    public void LightVibration()
-    {
-        Debug.Log("Light Vibration performed!");
-        //HapticFeedback.LightFeedback();
-    }
-
-    public void MediumVibration()
-    {
-        Debug.Log("Meduim Vibration performed!");
-        //HapticFeedback.MediumFeedback();
-    }
-
-    public void HeavyVibration()
-    {
-        Debug.Log("Heavy Vibration performed!");
-        //HapticFeedback.HeavyFeedback();
-    }
-#else
-        return;
-#endif
 
 
     
