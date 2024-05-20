85629f447a69f1f0cca55cb78d3957321b4445e4
Carlroos00 <carl.roos@hotmail.com>
2024-05-20 11:58:07 +0200

    Credit knapp + panel

--- MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/MenuController.cs
+++ MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/MenuController.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
 {
     //[SerializeField] private int levelToLoad;
     [SerializeField] private GameObject tutorial;
+    [SerializeField] private GameObject credits;
+
     public void StartGame()
     {
          SceneManager.LoadSceneAsync(1); // Här kan vi skriva in vad leveln heter, "Level 1", ELLER siffran på SceneBuildIndex. När man klickar på Play, ska leveln som står köras.
@@ -31,5 +33,15 @@ public class MainMenu : MonoBehaviour
     public void CloseTutorial()
     {
         tutorial.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        credits.SetActive(false);
     }
 }
