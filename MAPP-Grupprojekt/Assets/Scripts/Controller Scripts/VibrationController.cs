de581b00fa9da381e213cd79f0ec2725dee8e51d
weebchiro <sarita.afshar@gmail.com>
2024-05-21 14:41:30 +0200

    transition

--- MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/VibrationController.cs
+++ MAPP-Grupprojekt/Assets/Scripts/Controller Scripts/VibrationController.cs
@@ -9,6 +9,33 @@ public class VibrationController : MonoBehaviour
 {
 
 
+public void DefaultVibration()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        Debug.Log("Default Vibration performed!");
+            Handheld.Vibrate();
+    }
+
+    public void LightVibration()
+    {
+        Debug.Log("Light Vibration performed!");
+        //HapticFeedback.LightFeedback();
+    }
+
+    public void MediumVibration()
+    {
+        Debug.Log("Meduim Vibration performed!");
+        //HapticFeedback.MediumFeedback();
+    }
+
+    public void HeavyVibration()
+    {
+        Debug.Log("Heavy Vibration performed!");
+        //HapticFeedback.HeavyFeedback();
+    }
+#else
+        return;
+#endif
 
 
     
